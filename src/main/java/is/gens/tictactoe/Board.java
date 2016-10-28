a319ec2fa1e276668d80ebe716f8e43cc3553e77
Ragnarstefanss <ragnars15@ru.is>
2016-10-28 11:05:02 +0000

    added function can_move that checks if player can make a move on the board

--- src/main/java/is/gens/tictactoe/Board.java
+++ src/main/java/is/gens/tictactoe/Board.java
@@ -12,19 +12,18 @@ public class Board {
 	     }
     }
 
+
     public static void displayBoard(char[][] table) {
     	int rows_size = 3;
     	int column_size = 3;
 
     	if(table[0][0] != ' ')
     	{
-    		for (int row = 0; row < rows_size; row++)
-	    	{
-		        for (int col = 0; col < column_size; col++)
-		    	{
-		    		System.out.println(table[rows_size][column_size]);
-		    	}
-	    	}
+		    System.out.println( table[0][0] + " | " + table[0][1] + " | " + table[0][2] );
+		    System.out.println("---------");
+		    System.out.println( table[1][0] + " | " + table[1][1] + " | " + table[1][2] );
+		    System.out.println("---------");
+		    System.out.println( table[2][0] + " | " + table[2][1] + " | " + table[2][2] );
     	}
 	    else
 	    {
@@ -32,4 +31,16 @@ public class Board {
 	    }
 
 	}	
+
+	public static boolean can_move(char table[][], char player_o, char player_x) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (table[i][j] != player_x && table[i][j] != player_o)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 }
--- src/main/java/is/gens/tictactoe/TicTacToe.java
+++ src/main/java/is/gens/tictactoe/TicTacToe.java
@@ -5,10 +5,16 @@ public class TicTacToe {
     	int numRows = 3;
     	int numColumn = 3;
 		char[][] table = new char[numRows][numColumn];
+
+		char player_o = 'O';
+		char player_x = 'X';
 		
 		Board board = new Board();
 		board.initialize_table(table, numRows, numColumn);
 
 		board.displayBoard(table);
+
+		do {
+		} while(board.can_move(table, player_o, player_x));
     }
 }
